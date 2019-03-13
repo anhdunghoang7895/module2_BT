6485118fde08012ba43d7ee27bf21b9f5a374b9d
anhdunghoang7895 <anhdunghoang7895@gmail.com>
2019-03-13 11:37:59 +0700

    update

--- week_3/blog2.0/database/migrations/2014_10_12_000000_create_users_table.php
+++ week_3/blog2.0/database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration {
 
 			$table->string('password');
 
-			$table->string('admin');
+			$table->string('admin')->default(0);
 
 			$table->rememberToken();
 
